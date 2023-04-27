04e0c0b7219f64d2e109a1bfbe482759f9bd7af6
menna_ay <m.ayman@penta-b.com>
2023-04-27 21:15:47 +0200

    add comments

--- ServerSide/connections.js
+++ ServerSide/connections.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const express=require('express')
 const{DB_USERNAME,DB_PASSWORD,DB_NAME}=require("./configuration")
 const app=express()
-// connecting to the Database
+// connecting to the Database using env variables
 mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.wtbncmz.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`).then(() => {
     app.listen(3000)
 }).catch(()=>{
